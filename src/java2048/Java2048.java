1afdc12562e18061444339126c96f28b7b49b637
Grzegorz Grzęda <grzegorz.grzeda@m2mteam.pl>
2017-04-13 15:49:58 +0200

    Added simple factory like behavior in main.
    Now the player may choose to run the console view if desires.

--- src/java2048/Java2048.java
+++ src/java2048/Java2048.java
@@ -1,7 +1,6 @@
 package java2048;
 
 public class Java2048 {
-
 	private static int getGameFieldSize(String[] args) {
 		int side = 4;
 		if (args.length > 0) {
@@ -14,9 +13,20 @@ public class Java2048 {
 		return side;
 	}
 
+	private static IGameView getView(String[] args, int side) {
+		if (args.length > 1) {
+			try {
+				if (args[1].contains("c"))
+					return new ConsoleView();
+			} catch (NumberFormatException e) {
+			}
+		}
+		return new GameView(side);
+	}
+
 	public static void main(String[] args) {
 		int side = getGameFieldSize(args);
-		IGameView gv = new ConsoleView();// new GameView(side);
+		IGameView gv = getView(args, side);
 		IGameModel gm = new GameModel(side);
 		new GameController(gv, gm);
 	}
